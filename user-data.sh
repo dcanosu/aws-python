32a1f9de44bb3fa4b777476fd057068b3b8329d7
Dcanosu <dcanosu@gmail.com>
2026-08-01 19:23:39 -0500

    feat: Add user-date

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# cat /var/log/cloud-init-output.log
+
 # 1. Update installed packages non-interactively
 yum update -y
 
@@ -17,5 +19,10 @@ cd /home/ec2-user/aws-python
 
 # 6. Create virtual environment and install packages as ec2-user
 sudo -u ec2-user python -m venv .env
-sudo -u ec2-user source .env/bin/activate
-pip install -r requirements.txt
+
+# 7. Upgrade pip and install packages DIRECTLY using the venv pip executable
+sudo -u ec2-user ./.env/bin/pip install --upgrade pip
+sudo -u ec2-user ./.env/bin/pip install -r requirements.txt
+
+# 8. Start your Flask app in the background as ec2-user
+sudo -u ec2-user nohup ./.env/bin/python app.py > /home/ec2-user/app.log 2>&1 &
